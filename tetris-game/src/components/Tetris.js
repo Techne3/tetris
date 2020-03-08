ca85289f1e9af836a043b9015b0dfbada41af0c7
Timothy Sharkey <Techne3@gmail.com>
2020-03-08 17:54:04 -0600

    styled the layout for the game map added styled compoents to cell, stage, and tetris

--- tetris-game/src/components/Tetris.js
+++ tetris-game/src/components/Tetris.js
@@ -1,25 +1,28 @@
 import React from "react";
 
-// stopped at 42:12
+// stopped at 47:12
 
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
 import { createStage } from "../gameHelpers";
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 function Tetris() {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 }
 
